6e4fd315689ae78b6b4831199f077c8713441ae0
oniatsu <pblcusr@gmail.com>
2014-09-15 13:35:10 +0900

    おめでとうボタンをタップすると褒める

--- src/com/example/new1/MainActivity.java
+++ src/com/example/new1/MainActivity.java
@@ -1,25 +1,53 @@
 package com.example.new1;
 
+import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import jp.co.yahoo.android.maps.*;
 
-   
-public class MainActivity extends MapActivity {
+public class MainActivity extends MapActivity implements OnClickListener {
 	
 	private final String mAppId = "dj0zaiZpPUJCWkJyRzl3QUtvOCZzPWNvbnN1bWVyc2VjcmV0Jng9NDU-";
+
+	private RatingBar mRatingBar;
+	private Button mButton;
 
     @Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        // 地図とレイアウトを表示
         showMapWithLayout();
-//        showOnlyMap();
+        // showOnlyMap();
+
+        // Viewを取得
+        mRatingBar = (RatingBar) findViewById(R.id.ratingBar1);
+        mButton = (Button) findViewById(R.id.button1);
+
+        // クリックイベントを設定
+        mButton.setOnClickListener(this);
+    }
+    
+    @Override
+    public void onClick(View v) {
+    	switch (v.getId()) {
+		case R.id.button1:
+			Toast.makeText(this, "ももこさん " + mRatingBar.getRating() + " スコアおめでとう", Toast.LENGTH_SHORT).show();
+			break;
+		default:
+			break;
+		}
     }
 
     /**
@@ -47,7 +75,6 @@ public class MainActivity extends MapActivity {
         c.setZoom(1);                                 //初期表示の縮尺を指定
         setContentView(mapView);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
